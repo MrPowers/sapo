2eb3169c9851b3aa2352700d4c3bc8ae87e75552
MrPowers <matthewkevinpowers@gmail.com>
2020-09-20 13:30:32 -0500

    Add sqlite table and column validators

---
+++ sapo/sqlite_validator.py
@@ -0,0 +1,30 @@
+
+class SqliteValidator:
+    def __init__(self, conn):
+        self.conn = conn
+        self.cursor = conn.cursor()
+
+
+    def table_schema(self, table_name):
+        return self.cursor.execute(f"""PRAGMA table_info({table_name})""").fetchall()
+
+
+    def tables(self):
+        tables = self.cursor.execute("""SELECT name FROM sqlite_master WHERE type='table'""").fetchall()
+        return list(map(lambda r: r[0], tables))
+
+
+    # def only_contains_tables(tables):
+
+
+    def contains_column(self, table_name, column_name, data_type = None, nullable = None, default_value = None):
+        col = next((i for i in self.table_schema(table_name) if i[1] == column_name), None)
+        if not col:
+            return False
+        if data_type and col[2] != data_type:
+            return False
+        if nullable and col[3] != nullable:
+            return False
+        if default_value and col[4] != default_value:
+             return False
+        return True
---
+++ tests/create_sqlite_databases.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sqlite3
+import os
+
+def create_nfl_database():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, './data/nfl.db')
+
+    conn = sqlite3.connect(filename)
+    c = conn.cursor()
+
+    # create sqlite database tables
+    c.execute('''CREATE TABLE players (id int, last_name text, position int)''')
+    c.execute('''CREATE TABLE teams (id int, team_name text, team_city text)''')
+
+    # create DataFrames
+    player_data = [(1, 'gronk', 'tight_end'),
+        (2, 'brady', 'qb'),
+        (3, 'newton', 'qb')]
+    players_df = pd.DataFrame.from_records(player_data, columns=['id', 'last_name', 'position'])
+
+    teams_data = [(1, 'bucs', 'tampa_bay'),
+        (2, 'jets', 'new_york'),
+        (3, 'cheifs', 'kansas_city')]
+    teams_df = pd.DataFrame.from_records(teams_data, columns=['id', 'team_name', 'team_city'])
+
+    # load DataFrames into sqlite database
+    players_df.to_sql('players', conn, if_exists='append', index = False)
+    teams_df.to_sql('teams', conn, if_exists='append', index = False)
+
+create_nfl_database()
