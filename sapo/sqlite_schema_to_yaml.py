1f6263d1a972e250d610d116690cc7499bca7f82
MrPowers <matthewkevinpowers@gmail.com>
2020-09-30 12:08:17 -0400

    Create a more elegant function to output the sqlite schema as yaml

--- sapo/sqlite_schema_to_yaml.py
+++ sapo/sqlite_schema_to_yaml.py
@@ -1,27 +1,10 @@
 import yaml
 
-# sqlite GUIs let you copy the create statement to a string
-# The CREATE TABLE syntax is like this
-# CREATE TABLE players (id int, last_name text, position text)
-# The argument to this function should be the "id int, last_name text, position text" part
-
-def sqlite_create_table_to_yaml(table_name, cols, output_path):
-    # data = dict(
-        # A = 'a',
-        # B = dict(
-            # C = 'c',
-            # D = 'd',
-            # E = 'e',
-        # )
-    # )
-
-    data = {table_name: {}}
-
-    for col in cols.split(", "):
-        col_name, data_type = col.split(" ")
-        data[table_name][col_name] = {"data_type": data_type}
+from sapo.sqlite_helpers import SqliteHelpers
 
 
+def sqlite_db_schema_to_yaml(conn, output_path):
+    schema = SqliteHelpers(conn).schema()
     with open(output_path, 'w') as outfile:
-        yaml.dump(data, outfile, default_flow_style=False)
+        yaml.dump(schema, outfile, default_flow_style=False)
 
--- tests/test_sqlite_schema_to_yaml.py
+++ tests/test_sqlite_schema_to_yaml.py
@@ -1,12 +1,14 @@
 import pytest
 import os
+import sqlite3
 
 from sapo.sqlite_schema_to_yaml import *
 
-def test_sqlite_create_table_to_yaml():
-    dirname = os.path.dirname(__file__)
-    filename = os.path.join(dirname, '../tmp/nfl_players_schema.yml')
+dirname = os.path.dirname(__file__)
 
-    cols = "id int, last_name text, position text"
-    sqlite_create_table_to_yaml('players', cols, filename)
+def test_sqlite_db_schema_to_yaml():
+    filename = os.path.join(dirname, './data/nfl.db')
+    conn = sqlite3.connect(filename)
+    output_path = os.path.join(dirname, '../tmp/nfl_db_schema.yml')
+    sqlite_db_schema_to_yaml(conn, output_path)
 
